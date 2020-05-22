66780227d8a3489ac61a1e15a08b0a2e813ceea6
Nick Nicholas <opoudjis@gmail.com>
2020-05-22 11:54:41 +1000

    update

--- xapi_observations/lpofai_observations.rb
+++ xapi_observations/lpofai_observations.rb
@@ -41,23 +41,13 @@ def get_indicators(s, progs, n)
   ret.map { |i| progs[s[:yrlvl]][i] }
 end
 
-=begin
-students = []
-File.open("students.txt", "r") do |f|
-  f.each_line do |l|
-    a = l.strip.split(/,/)
-    students << {name: a[0], email: a[1], yrlvl: a[2] }
-  end
-end
-=end
-
 def get_sif_students
   students = []
   gen = Rubystats::NormalDistribution.new(0.5, 0.18)
   sessiontoken = "ca6618e3-f17a-42cb-90be-6543b016f489"
   usertoken = "7417b5beee404467b5ac6f9584ee6ec9"
   tk = Base64.encode64("#{sessiontoken}:#{usertoken}").strip.gsub(/\n/, "")
-  url = "http://hits.nsip.edu.au/SIF3InfraREST/hits/requests/StudentPersonals?access_token=#{tk}&authenticationMethod=Basic&timestamp=#{Time.now.iso8601}&navigationPage=1&navigationPageSize=100"
+  url = "http://hits.nsip.edu.au/SIF3InfraREST/hits/requests/StudentPersonals?access_token=#{tk}&authenticationMethod=Basic&timestamp=#{Time.now.iso8601}&navigationPage=1&navigationPageSize=1000"
   xml = `curl "#{url}"`
   xml1 = Nokogiri::XML(xml)
   xml1.xpath("//xmlns:StudentPersonal").each do |x|
@@ -73,65 +63,254 @@ def get_sif_students
   students
 end
 
-students = get_sif_students()
+def teachers 
+  {
+    "1": {name: "Lizabeth Portwood", email: "lp@example.com" },
+    "2": {name: "Suk Boger", email: "sb@example.com" },
+    "3": {name: "Shara Posner", email: "sp@example.com" },
+    "4": {name: "Marietta Maley", email: "mm@example.com" },
+    "5": {name: "Vickie Ritzer", email: "vr@example.com" },
+    "6": {name: "Marina Paulino", email: "mp@example.com" },
+    "7": {name: "Nam Tome", email: "nt@example.com" },
+    "8": {name: "Alton Newbill", email: "an@example.com" },
+    "9": {name: "Janice Dedrick", email: "jd@example.com" },
+    "10": {name: "Marya Sieben", email: "ms@example.com" },
+    "11": {name: "Julissa Fewell", email: "jf@example.com" },
+    "12": {name: "Margart Hunsicker", email: "mh@example.com" }
+  }
+end
+
+def observation(i,s)
+  r = get_result(s)
+  {
+    id: UUIDTools::UUID.random_create,
+    actor: {
+      name: s[:name],
+      mbox: "mailto:" + s[:email],
+      objectType: "Agent"
+    },
+    verb: {
+      id: "http://adlnet.gov/expapi/verbs/mastered",
+      display: {
+        "en-US": "mastered"
+      }
+    },
+    object: {
+      id: i[:id],
+      definition: {
+        name: {
+          "en-US": i[:code],
+        },
+        description: {
+          "en-US": i[:text]
+        },
+        type: "http://adlnet.gov/expapi/activities/objective"
+      },
+      objectType: "Activity"
+    },
+    result: {
+      success: (r == "mastered" ? "true" : "false"),
+      duration: "P30D",
+      extensions: {
+        # https://github.com/adlnet/xapi-authored-profiles/blob/master/tincan/tincan.ttl
+        "http://www.tincanapi.co.uk/extensions/result/classification": {
+          id: UUIDTools::UUID.random_create,
+          definition: {
+            name: {
+              "en-US": r
+            },
+            "type": "http://www.tincanapi.co.uk/activitytypes/grade_classification"
+          },
+          objectType: "Activity"
+        }
+      }
+    },
+    timestamp: "2020-05-01T23",
+  }
+end
+
+def assessment(i, s, n)
+  {
+    id: UUIDTools::UUID.random_create,
+    actor: {
+      name: s[:name],
+      mbox: "mailto:" + s[:email],
+      objectType: "Agent"
+    },
+    verb: {
+      id: "http://adlnet.gov/expapi/verbs/completed",
+      display: {
+        "en-US": "completed"
+      }
+    },
+    object: {
+      id: "http://www.example.com/assessment/#{n}",
+      definition: {
+        name: {
+          "en-US": "Assessment for #{i[:code]}",
+        },
+        extensions: {
+          "http://id.tincanapi.com/extension/topic": "general"
+        },
+        type: "http://adlnet.gov/expapi/activities/assessment"
+      },
+      objectType: "Activity"
+    },
+    context: {
+      instructor: {
+        name: teachers[s[:yrlvl].to_sym][:name],
+        mbox: "mailto:" + teachers[s[:yrlvl].to_sym][:email],
+        objectType: "Agent"
+      }
+    },
+    result: {
+      success: (r == "mastered" ? "true" : "false"),
+      duration: "P30D",
+      extensions: {
+        "http://www.tincanapi.co.uk/extensions/result/classification": {
+          id: UUIDTools::UUID.random_create,
+          definition: {
+            name: {
+              "en-US": r
+            },
+            "type": "http://www.tincanapi.co.uk/activitytypes/grade_classification"
+          },
+          objectType: "Activity"
+        }
+      }
+    },
+    timestamp: "2020-05-01T23",
+  }
+end
+
+def grade(s)
+  r = s[:aptitude] + 0.2 + (rand  - 0.5)*0.5
+  r = 1 if r > 1
+  r = 0 if r < 0
+  ret = if r >= 0.8 then "A"
+        elsif r >= 0.7 then "B"
+        elsif r >= 0.6 then "C"
+        elsif r >= 0.5 then "D"
+        elsif r >= 0.4 then "E"
+        else
+          "F"
+        end
+  [(r * 100).round, ret]
+end
+
+def assessment1(i, s, n)
+  numbergrade, lettergrade = grade(s)
+  {
+    id: UUIDTools::UUID.random_create,
+    actor: {
+      name: s[:name],
+      mbox: "mailto:" + s[:email],
+      objectType: "Agent"
+    },
+    verb: {
+      id: "http://activitystrea.ms/schema/1.0/satisfy",
+      display: {
+        "en-US": "satisfied"
+      }
+    },
+    object: {
+      id: i[:id],
+      definition: {
+        name: {
+          "en-US": i[:code],
+        },
+        description: {
+          "en-US": i[:text]
+        },
+        type: "http://adlnet.gov/expapi/activities/objective"
+      },
+      objectType: "Activity"
+    },
+    context: {
+      instructor: {
+        name: teachers[s[:yrlvl].to_sym][:name],
+        mbox: "mailto:" + teachers[s[:yrlvl].to_sym][:email],
+        objectType: "Agent"
+      },
+      extensions: {
+        "http://id.tincanapi.com/extension/target": {
+          id: "http://www.example.com/assessment/#{n}",
+          definition: {
+            name: {
+              "en-US": "Assessment for #{i[:code]}",
+            },
+            extensions: {
+              "http://id.tincanapi.com/extension/topic": "general"
+            },
+            type: "http://adlnet.gov/expapi/activities/assessment"
+          },
+          objectType: "Activity"
+        }
+      }
+    },
+    result: {
+      success: (lettergrade <= "D" ? "true" : "false"),
+      score: {
+        scaled: numbergrade,
+        raw: numbergrade,
+        min: 0,
+        max: 100,
+      },
+      duration: "P30D",
+      extensions: {
+        "http://www.tincanapi.co.uk/extensions/result/classification": {
+          id: UUIDTools::UUID.random_create,
+          definition: {
+            name: {
+              "en-US": lettergrade
+            },
+            "type": "http://www.tincanapi.co.uk/activitytypes/grade_classification"
+          },
+          objectType: "Activity"
+        }
+      }
+    },
+    timestamp: "2020-05-01T23",
+  }
+end
 
 lp = JSON.parse(File.read("progressions.json"))
 lp.each do |s|
   lp_traverse(s, nil, nil, progs)
 end
 
-#warn progs
-
-observations = []
+students = get_sif_students()
+students_per_yrlvl = {}
 students.each do |s|
-  get_indicators(s, progs, 10).each do |i|
-    r = get_result(s)
-    observations << {
-      id: UUIDTools::UUID.random_create,
-      actor: {
-        name: s[:name],
-        mbox: s[:email],
-        objectType: "Person"
-      },
-      verb: {
-        id: "http://adlnet.gov/expapi/verbs/mastered",
-        display: {
-          "en-US": "mastered"
-        }
-      },
-      object: {
-        id: i[:id],
-        definition: {
-          name: {
-            "en-US": i[:code],
-          },
-          description: {
-            "en-US": i[:text]
-          },
-          type: "http://adlnet.gov/expapi/activities/objective"
-        },
-        objectType: "Activity"
-      },
-      result: {
-        success: (r == "mastered" ? "true" : "false"),
-        duration: "P30D",
-        extensions: {
-          # https://github.com/adlnet/xapi-authored-profiles/blob/master/tincan/tincan.ttl
-          "http://www.tincanapi.co.uk/extensions/result/classification": {
-            id: UUIDTools::UUID.random_create,
-            definition: {
-              name: {
-                "en-US": r
-              }
-            },
-            objectType: "http://www.tincanapi.co.uk/activitytypes/grade_classification"
-          }
-        }
-      },
-      timestamp: "2020-05-01T23",
-    }
-  end
+  students_per_yrlvl[s[:yrlvl]] ||= []
+  students_per_yrlvl[s[:yrlvl]] << s
 end
 
-puts JSON.pretty_generate(observations)
+
+def NSIP_observations
+  observations = []
+  students.each do |s|
+    get_indicators(s, progs, 10).each do |i|
+      observations << observation(i, s)
+    end
+  end
+
+  students_per_yrlvl.keys.each do |yr|
+    get_indicators(students_per_yrlvl[yr][0], progs, 10).each_with_index do |i, n|
+      students_per_yrlvl[yr].each do |s|
+        observations << assessment1(i, s, n)
+      end
+    end
+  end
+  observations
+end
+
+def ESA_observations()
+  gemma = {name: "Gemma Boger", email: "gb@example.com" } # Yr 2 English
+  ruby = {name: "Ruby Tome", email: "rt@example.com" } # Yr 7 & 8 Maths
+  jet = {name: "Jet Zhang", email: "jz@example.com" } # Yr 3 Maths & English
+end
+
+#puts JSON.pretty_generate(NSIP_observations())
+puts JSON.pretty_generate(ESA_observations())
 
